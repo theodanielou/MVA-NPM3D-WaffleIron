ab96f9ad0afd41a7d7ac8a74afe7e63f8ad35ccd
Théo Danielou <theo.danielou@telecom-sudparis.eu>
2024-03-09 12:27:59 +0100

    training

--- datasets/waymo.py
+++ datasets/waymo.py
@@ -13,13 +13,13 @@ class WaymoSemSeg(PCDataset):
             os.path.join(current_folder, "list_files_waymo.npz") # à créer
         )[self.phase]
         if self.phase == "train":
-            assert len(self) == 142
+            assert len(self) == 3736
         elif self.phase == "val":
-            assert len(self) == 36
+            assert len(self) == 934
         # elif self.phase == "test":
         #     assert len(self) == 6008
         elif self.phase == "test":
-            assert len(self) == 20
+            assert len(self) == 519
         else:
             raise ValueError(f"Unknown phase {self.phase}.")
 
